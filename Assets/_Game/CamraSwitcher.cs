f839d1057ada613675b5954a571c35c542f7be4e
Wanabii <aleksandrovtimothy@gmail.com>
2025-05-31 21:04:07 +0200

    added camera positions

---
+++ Assets/_Game/CamraSwitcher.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    public CinemachineCamera vcMain;
+    public CinemachineCamera vcFront;
+    public CinemachineCamera vcBack;
+
+    private int _switcher = 0;
+
+    void Start()
+    {
+        SetCamera(vcMain); // при старте активируем главную
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)&&_switcher<1)
+        {
+            _switcher++;
+        }
+        else if (Input.GetKeyDown(KeyCode.S)&&_switcher>-1)
+        {
+            _switcher--;
+        }
+
+        switch (_switcher)
+        {
+            case 0:
+                SetCamera(vcMain);
+                break;
+            case 1:
+                SetCamera(vcFront);
+                break;
+            case -1:
+                SetCamera(vcBack);
+                break;
+        }
+        
+    }
+
+    void SetCamera(CinemachineCamera activeCam)
+    {
+        // Сброс всех приоритетов
+        vcMain.Priority = 0;
+        vcFront.Priority = 0;
+        vcBack.Priority = 0;
+
+        // Устанавливаем активной
+        activeCam.Priority = 10;
+    }
+}
